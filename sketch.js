880ce7f20d30a9716e5d48685819892e11b8c245
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-01-03 11:36:01 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,20 +5,20 @@ const Bodies = Matter.Bodies;
 let engine;
 let world;
 
-var ground;
-var left;
-var right;
-var top_wall;
+var solo;
+var parede_esq;
+var parede_dir;
+var parede_cima;
 
 function setup() {
   createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
   
-  ground =new Ground(200,390,400,20);
-  right = new Ground(390,200,20,400);
-  left = new Ground(10,200,20,400);
-  top_wall = new Ground(200,10,400,20);
+  solo =new Solo(200,390,400,20);
+  parede_dir = new Solo(390,200,20,400);
+  parede_esq = new Solo(10,200,20,400);
+  parede_cima = new Solo(200,10,400,20);
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -26,10 +26,10 @@ function setup() {
 
 function draw() {
   background(51);
-  ground.show();
-  top_wall.show();
-  left.show();
-  right.show();
+  solo.mostrar();
+  parede_cima.mostrar();
+  parede_esq.mostrar();
+  parede_dir.mostrar();
   Engine.update(engine);
 }
 
